2400b0f993c143fa973c5ca9119498a449e832f4
Vicentiu Petreaca <vicentiu.petreaca@geminisols.ro>
2019-06-18 18:04:12 +0300

    added all planets

--- ARSolarSystem/MainViewController.swift
+++ ARSolarSystem/MainViewController.swift
@@ -10,18 +10,27 @@ import UIKit
 import ARKit
 
 enum PlanetIndex: Int, CaseIterable {
-    case mercury = 0, venus, earth, moon, mars//, jupiter, saturn, uranus, neptune
+    case mercury = 0, venus, earth, moon, mars, jupiter, saturn, uranus, neptune
 }
 
 class MainViewController: UIViewController {
-    
-    @IBOutlet weak var sceneView: ARSCNView!
-    
     let configuration = ARWorldTrackingConfiguration()
+
+    @IBOutlet weak var sceneView: ARSCNView!
+
+    @IBAction func tapped(_ sender: UITapGestureRecognizer) {
+        if sender.state == .ended {
+            let location = sender.location(in: sceneView)
+            let hits = sceneView.hitTest(location, options: nil)
+            if hits.isEmpty == false,
+                let planet = hits.first?.node as? PlanetWrapper {
+                print(planet.id, planet.name)
+            }
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(rec:)))
-        sceneView.addGestureRecognizer(tap)
         AVCaptureDevice.requestAccess(for: .video) {
             if $0 {
                 self.sceneView.session.run(self.configuration)
@@ -69,6 +78,26 @@ class MainViewController: UIViewController {
                                                    diffuse: #imageLiteral(resourceName: "Mars Surface"),
                                                    position: SCNVector3(7.2, yPos, zPos),
                                                    model: PlanetModel(id: "0", name: "Mars")))
+            case .jupiter:
+                $0[$1] = (SCNNode(), PlanetWrapper(geometry: SCNSphere(radius: 0.69),
+                                                   diffuse: #imageLiteral(resourceName: "Jupiter Surface"),
+                                                   position: SCNVector3(8.6, yPos, zPos),
+                                                   model: PlanetModel(id: "3", name: "Jupiter")))
+            case .saturn:
+                $0[$1] = (SCNNode(), PlanetWrapper(geometry: SCNSphere(radius: 0.6),
+                                                   diffuse: #imageLiteral(resourceName: "Saturn Surface"),
+                                                   position: SCNVector3(10, yPos, zPos),
+                                                   model: PlanetModel(id: "63", name: "Saturn")))
+            case .uranus:
+                $0[$1] = (SCNNode(), PlanetWrapper(geometry: SCNSphere(radius: 0.25),
+                                                   diffuse: #imageLiteral(resourceName: "Uranus Surface"),
+                                                   position: SCNVector3(10.5, yPos, zPos),
+                                                   model: PlanetModel(id: "323", name: "Uranus")))
+            case .neptune:
+                $0[$1] = (SCNNode(), PlanetWrapper(geometry: SCNSphere(radius: 0.24),
+                                                   diffuse: #imageLiteral(resourceName: "Neptune Surface"),
+                                                   position: SCNVector3(11, yPos, zPos),
+                                                   model: PlanetModel(id: "36", name: "Neptune")))
             }
         }
 
@@ -78,11 +107,15 @@ class MainViewController: UIViewController {
             .filter { $0.key != .moon }
             .forEach { sceneView.scene.rootNode.addChildNode($0.value.parent) }
 
-        createPlanet(.mercury, dict: planetDict, selfRotation: rotation(time: 3), axisRotation: rotation(time: 7))
-        createPlanet(.venus, dict: planetDict, selfRotation: rotation(time: 4), axisRotation: rotation(time: 7))
-        createPlanet(.earth, dict: planetDict, selfRotation: rotation(time: 5), axisRotation: rotation(time: 9))
-        createPlanet(.moon, dict: planetDict, selfRotation: rotation(time: 5), axisRotation: rotation(time: 5))
-        createPlanet(.mars, dict: planetDict, selfRotation: rotation(time: 6), axisRotation: rotation(time: 11))
+        createPlanet(.mercury, from: planetDict, selfRotation: rotation(time: 3), axisRotation: rotation(time: 7))
+        createPlanet(.venus, from: planetDict, selfRotation: rotation(time: 4), axisRotation: rotation(time: 7))
+        createPlanet(.earth, from: planetDict, selfRotation: rotation(time: 5), axisRotation: rotation(time: 9))
+        createPlanet(.moon, from: planetDict, selfRotation: rotation(time: 5), axisRotation: rotation(time: 5))
+        createPlanet(.mars, from: planetDict, selfRotation: rotation(time: 6), axisRotation: rotation(time: 11))
+        createPlanet(.jupiter, from: planetDict, selfRotation: rotation(time: 7), axisRotation: rotation(time: 13))
+        createPlanet(.saturn, from: planetDict, selfRotation: rotation(time: 8), axisRotation: rotation(time: 15))
+        createPlanet(.uranus, from: planetDict, selfRotation: rotation(time: 8), axisRotation: rotation(time: 17))
+        createPlanet(.neptune, from: planetDict, selfRotation: rotation(time: 8), axisRotation: rotation(time: 19))
 
         planetDict.forEach { animateIn(node: $0.value.wrapper) }
 
@@ -95,7 +128,7 @@ class MainViewController: UIViewController {
 
     private func createPlanet(
         _ `enum`: PlanetIndex,
-        dict: [PlanetIndex: (parent: SCNNode, wrapper: PlanetWrapper)],
+        from dict: [PlanetIndex: (parent: SCNNode, wrapper: PlanetWrapper)],
         selfRotation: SCNAction,
         axisRotation: SCNAction) {
         dict[`enum`]?.wrapper.runAction(selfRotation)
@@ -111,16 +144,5 @@ class MainViewController: UIViewController {
     private func rotation(time: TimeInterval) -> SCNAction {
         let rotation = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: time * 2)
         return SCNAction.repeatForever(rotation)
-    }
-
-    @objc func handleTap(rec: UITapGestureRecognizer) {
-        if rec.state == .ended {
-            let location = rec.location(in: sceneView)
-            let hits = sceneView.hitTest(location, options: nil)
-            if !hits.isEmpty,
-                let tappedNode = hits.first?.node as? PlanetWrapper {
-                print(tappedNode.id, tappedNode.name)
-            }
-        }
     }
 }
